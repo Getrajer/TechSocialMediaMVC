c0444e29841f29a93d84a31658707dac25318ade
getrajer533 <getrajer533@gmail.com>
2020-10-04 17:37:06 +0100

    more f

---
+++ TechSocialMedia/Controllers/FeedController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechSocialMedia.Controllers
+{
+    public class FeedController : Controller
+    {
+        public IActionResult MainFeed()
+        {
+            return View();
+        }
+    }
+}
